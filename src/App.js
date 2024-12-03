dc98354afce803f3c609c5fe0783993740f13486
git4roman <romanforgit@gmail.com>
2024-12-03 20:37:00 +0545

    homepage

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
   }, []);
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/portfolio">
       <ScrollToTop>
         <HeaderMain />
         <AppRoutes />
